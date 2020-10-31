05a067db6df52e3d14f477bbd35023dc4b13e892
Vera Vovk <vovkverait@gmail.com>
2020-10-31 14:45:00 +0200

    Add right category for tests

--- auctions/tests.py
+++ auctions/tests.py
@@ -3,10 +3,11 @@ from .models import Category
 class FlightTestCase(TestCase):
 
     def setUp(self):
-        c1 = Category.objects.create(group_name = "Books", description = "There are a lot of different books")
+        #c1 = Category.objects.create(group_name = "Books", description = "There are a lot of different books")
+        c1 = Category.objects.create(group_name = "A", description = "aa")
         c1.save()
 
 
     def test_departures_count(self):
-        c = Category.objects.get(group_name="Books")
+        c = Category.objects.get(group_name="A")
         self.assertEqual(c.description, "There are a lot of different books")
